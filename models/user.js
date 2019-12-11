93208b563470848a42879cda30d7935b4114d8d2
AGX Nishiyama <nishiyama.y@one-all.co.jp>
2019-12-11 09:25:21 +0900

    Userモデル追加

---
+++ models/user.js
@@ -0,0 +1,17 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      line_id: DataTypes.STRING,
+      note: DataTypes.STRING
+    },
+    {
+      underscored: true
+    }
+  );
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
