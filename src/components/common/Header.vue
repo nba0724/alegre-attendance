5c51d3dcc31a28c73795f1c0b135459b9455f0ea
AGX Nishiyama <nishiyama.y@one-all.co.jp>
2020-01-06 18:46:03 +0900

    ナビゲーションを作成

---
+++ src/components/common/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-app-bar app color="green accent-4" dark>
+      <v-toolbar-title>
+        <span>ALEGRE SCHEDULE</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <Menu v-model="drawer" />
+  </div>
+</template>
+
+<script>
+import Menu from "@/components/common/Menu";
+export default {
+  name: "Header",
+  components: {
+    Menu
+  },
+  data: () => {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
---
+++ src/components/common/Menu.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    class="green accent-4"
+    app
+    temporary
+    right
+    dark
+  >
+    <v-list>
+      <v-list-item
+        v-for="item in items"
+        :key="item.name"
+        link
+        @click="clickMenu(item)"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  computed: {
+    drawer: {
+      get() {
+        return this.value;
+      },
+      set(newVal) {
+        if (this.value !== newVal) this.$emit("input", newVal);
+      }
+    }
+  },
+  data: () => {
+    return {
+      items: [
+        {
+          name: "Home",
+          icon: "home",
+          url: "/home"
+        },
+        {
+          name: "Reservation",
+          icon: "today",
+          url: "/reservation"
+        }
+      ]
+    };
+  },
+  methods: {
+    clickMenu: async item => {
+      this.$router.push({ path: item.url });
+    }
+  }
+};
+</script>
