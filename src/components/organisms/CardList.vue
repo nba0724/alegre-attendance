3c0b64ce21e5e16936e798b8f76437961aab0a95
AGX Nishiyama <nishiyama.y@one-all.co.jp>
2020-01-07 10:22:53 +0900

    CardList作成

---
+++ src/components/organisms/CardList.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row v-for="list in listData" :key="list">
+      <v-col cols="12">
+        <Card :data="list" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Card from "@/components/molecules/Card";
+
+export default {
+  name: "cardList",
+  props: ["listData"],
+  components: { Card }
+};
+</script>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="home">aaa</div>
+  <CardList :listData="listData" />
 </template>
 
 <script>
+import CardList from "@/components/organisms/CardList";
 export default {
-  name: "home"
+  name: "home",
+  components: { CardList },
+  data: () => ({
+    listData: ["aaa", "bbb", "ccc"]
+  })
 };
 </script>
