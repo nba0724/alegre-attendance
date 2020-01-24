07ce67868e8ebde35bdd624cbad5897d49937f2c
nishiyama.y <nishiyama.y@one-all.co.jp>
2020-01-24 15:47:22 +0900

    スケジュール詳細画面　編集

---
+++ src/components/organisms/DateTime.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row no-gutters>
+    <v-col cols="6">
+      <v-menu
+        v-model="menu2"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        transition="scale-transition"
+        offset-y
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+            v-model="date"
+            :label="item.dateLabel"
+            prepend-inner-icon="today"
+            readonly
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+      </v-menu>
+    </v-col>
+    <v-col cols="6">
+      <v-text-field
+        v-model="time"
+        :label="item.timeLabel"
+        prepend-inner-icon="access_time"
+        readonly
+      ></v-text-field>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "dateTime",
+  props: ["item"],
+  data: () => ({
+    menu2: false,
+    date: new Date(),
+    time: ""
+  })
+};
+</script>
--- src/components/pages/ScheduleDetail.vue
+++ src/components/pages/ScheduleDetail.vue
@@ -6,23 +6,64 @@
       </v-col>
       <v-col cols="auto" class="mt-5"> <Button :item="buttonData"/></v-col>
     </v-row>
+    <v-row no-gutters>
+      <v-col>
+        <v-text-field v-model="subject" label="Subject" readonly></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row no-gutters><DateTime :item="startDateTimeData" /> </v-row>
+    <v-row no-gutters><DateTime :item="endDateTimeData" /> </v-row>
+    <v-row no-gutters>
+      <v-col cols="4">
+        <v-btn block color="green accent-4" dark>Going</v-btn>
+      </v-col>
+      <v-col cols="4"
+        ><v-btn block color="orange darken-1" dark>Maybe</v-btn>
+      </v-col>
+      <v-col cols="4">
+        <v-btn block color="blue-grey darken-1" dark>Can't Go</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="mt-5">
+      <v-col cols="12">
+        <p>参加者 2人・未定 1人</p>
+      </v-col>
+      <v-col cols="12" no-gutters>
+        <v-badge color="green" overlap>
+          <v-icon slot="badge" dark small>done</v-icon>
+          <v-avatar>
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          </v-avatar>
+        </v-badge>
+        <v-badge color="yellow" overlap>
+          <v-icon slot="badge" dark small>stop</v-icon>
+          <v-avatar>
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          </v-avatar>
+        </v-badge>
+      </v-col>
+    </v-row>
     <v-row>
-      <CardList :listData="listData" />
+      <v-col>
+        <v-textarea auto-grow label="Detail">aaa</v-textarea>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import ContentTitle from "@/components/organisms/ContentTitle";
-import CardList from "@/components/organisms/CardList";
+import DateTime from "@/components/organisms/DateTime";
 import Button from "@/components/molecules/Button";
 export default {
   name: "home",
-  components: { CardList, ContentTitle, Button },
+  components: { ContentTitle, DateTime, Button },
   data: () => ({
     title: "Schedule",
     buttonData: { icon: "edit", name: "", color: "green accent-4" },
-    listData: ["aaaAAAAAAAAAAAAAAAAAAA", "bbb", "ccc"]
+    startDateTimeData: { dateLabel: "Start Date", timeLabel: "Start Time" },
+    endDateTimeData: { dateLabel: "End Date", timeLabel: "End Time" },
+    subject: "aaaa"
   })
 };
 </script>
